3d5ecb89d51082e682a21f31156a66de2a860722
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2026-05-25 01:17:05 +0700

    feat: [fatih] add tailwind config ts

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx,css,scss}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
